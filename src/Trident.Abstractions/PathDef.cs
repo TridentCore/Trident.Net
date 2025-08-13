ccbe170d19cad3d158afc8ac7794b6ed111ebc08
d3ara1n <d3ara1n@outlook.com>
2025-08-13 17:24:37 +0800

    chore(PathDef): remove unused method

--- src/Trident.Abstractions/PathDef.cs
+++ src/Trident.Abstractions/PathDef.cs
@@ -99,13 +99,12 @@ namespace Trident.Abstractions
         public string DirectoryOfHome(string key) => Path.Combine(InstanceDirectory, key);
 
         public string DirectoryOfBuild(string key) => Path.Combine(InstanceDirectory, key, "build");
+        
         public string DirectoryOfNatives(string key) => Path.Combine(DirectoryOfBuild(key), "natives");
 
         public string DirectoryOfImport(string key) => Path.Combine(InstanceDirectory, key, "import");
 
         public string DirectoryOfPersist(string key) => Path.Combine(InstanceDirectory, key, "persist");
-
-        public string DirectoryOfSnapshots(string key) => Path.Combine(InstanceDirectory, key, "snapshots");
 
         #endregion
     }
