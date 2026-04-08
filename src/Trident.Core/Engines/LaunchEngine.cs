ef3c3a538d0928f876ce4b82ebf7c6b10e96290a
Dearain <d3ara1n@outlook.com>
2026-04-08 16:59:56 +0800

    refactor: 让 Scrap 解析不再依赖内部方法

--- src/Trident.Core/Engines/LaunchEngine.cs
+++ src/Trident.Core/Engines/LaunchEngine.cs
@@ -2,6 +2,7 @@ using System.Diagnostics;
 using System.Text.RegularExpressions;
 using System.Threading.Channels;
 using Trident.Core.Engines.Launching;
+using Trident.Core.Utilities;
 
 namespace Trident.Core.Engines;
 
@@ -34,7 +35,6 @@ public partial class LaunchEngine : IAsyncEnumerable<Scrap>
         private readonly CancellationToken _cancellationToken;
         private readonly Channel<Scrap> _channel = Channel.CreateUnbounded<Scrap>();
         private readonly Process _inner;
-        private readonly Regex _pattern = GenerateRegex();
 
         internal LaunchEngineEnumerator(Process process, CancellationToken token = default)
         {
@@ -97,7 +97,7 @@ public partial class LaunchEngine : IAsyncEnumerable<Scrap>
         {
             if (!string.IsNullOrEmpty(e.Data))
             {
-                _channel.Writer.TryWrite(TryConstruct(e.Data));
+                _channel.Writer.TryWrite(ScrapHelper.Parse(e.Data));
             }
         }
 
@@ -105,46 +105,12 @@ public partial class LaunchEngine : IAsyncEnumerable<Scrap>
         {
             if (!string.IsNullOrEmpty(e.Data))
             {
-                _channel.Writer.TryWrite(TryConstruct(e.Data));
+                _channel.Writer.TryWrite(ScrapHelper.Parse(e.Data));
             }
         }
 
         private void ProcessOnExited(object? sender, EventArgs e) => _channel.Writer.TryComplete();
 
-        private Scrap TryConstruct(string data)
-        {
-            var match = _pattern.Match(data);
-            if (
-                match.Success
-                && match.Groups.TryGetValue("level", out var level)
-                && match.Groups.TryGetValue("thread", out var thread)
-                && match.Groups.TryGetValue("message", out var message)
-            )
-            {
-                match.Groups.TryGetValue("source", out var sender);
-                return new(
-                    message.Value,
-                    level.Value.ToUpper() switch
-                    {
-                        "INFO" => ScrapLevel.Information,
-                        "WARN" => ScrapLevel.Warning,
-                        "ERROR" => ScrapLevel.Error,
-                        _ => ScrapLevel.Information,
-                    },
-                    DateTimeOffset.Now,
-                    thread.Value,
-                    sender?.Value
-                );
-            }
-
-            return new(data);
-        }
-
-        [GeneratedRegex(
-            @"\[(.*)\] \[(?<thread>[a-zA-Z0-9\ \-#@]+)/(?<level>[a-zA-Z]+)\](\ \[(?<source>[a-zA-Z0-9\ \\./\-]+)\])?: (?<message>.*)"
-        )]
-        private static partial Regex GenerateRegex();
-    }
-
     #endregion
+    }
 }
---
+++ src/Trident.Core/Utilities/ScrapHelper.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Text.RegularExpressions;
+using Trident.Core.Engines.Launching;
+
+namespace Trident.Core.Utilities;
+
+public static partial class ScrapHelper
+{
+    [GeneratedRegex(
+        @"\[(.*)\] \[(?<thread>[a-zA-Z0-9\ \-#@]+)/(?<level>[a-zA-Z]+)\](\ \[(?<source>[a-zA-Z0-9\ \\./\-]+)\])?: (?<message>.*)"
+    )]
+    private static partial Regex GenerateRegex();
+
+    private static Regex pattern = GenerateRegex();
+
+    public static Scrap Parse(string data)
+    {
+        var match = pattern.Match(data);
+        if (
+            match.Success
+            && match.Groups.TryGetValue("level", out var level)
+            && match.Groups.TryGetValue("thread", out var thread)
+            && match.Groups.TryGetValue("message", out var message)
+        )
+        {
+            match.Groups.TryGetValue("source", out var sender);
+            return new(
+                message.Value,
+                level.Value.ToUpper() switch
+                {
+                    "INFO" => ScrapLevel.Information,
+                    "WARN" => ScrapLevel.Warning,
+                    "ERROR" => ScrapLevel.Error,
+                    _ => ScrapLevel.Information,
+                },
+                DateTimeOffset.Now,
+                thread.Value,
+                sender?.Value
+            );
+        }
+
+        return new(data);
+    }
+}
